1be4bd3b5e305df6e7544419a30c6a0d910ada4f
StephensCheng <39405166+StephensCheng@users.noreply.github.com>
2018-11-08 19:58:52 +0800

    Create Contrastiveloss.py

---
+++ Contrastiveloss.py
@@ -0,0 +1,18 @@
+# 数据集之外的重点就是损失函数
+import torch
+import torch.nn.functional as F
+
+
+class ContrastiveLoss(torch.nn.Module):
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    # forward过程计算损失函数
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+        return loss_contrastive
